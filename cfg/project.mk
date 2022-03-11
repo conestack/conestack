ddba39503c6c44c58931a480d4e040adb22ef746
Robert Niederreiter <rnix@squarewave.at>
2022-03-11 10:34:57 +0100

    Improve openldap targets. One file for project related targets and settings

---
+++ cfg/project.mk
@@ -0,0 +1,30 @@
+###############################################################################
+# project settings
+###############################################################################
+
+SYSTEM_DEPENDENCIES=\
+	build-essential \
+	curl \
+	libsasl2-dev \
+	libssl-dev \
+	libdb-dev \
+	libltdl-dev
+
+###############################################################################
+# install
+###############################################################################
+
+.PHONY: project-install
+project-install: python-ldap
+	@make install
+
+
+###############################################################################
+# clean
+###############################################################################
+
+.PHONY: project-clean
+project-clean: clean openldap-clean
+
+.PHONY: project-full-clean
+project-clean: full-clean openldap-clean
